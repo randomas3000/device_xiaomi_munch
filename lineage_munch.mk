4aeb711a00b8b8d3f91e218c1b92df4d05420cd2
Akash Kakkar <akash.galaxy07@gmail.com>
2022-10-08 03:15:20 +0530

    Lineage/crDroid bringup

--- lineage_munch.mk
+++ lineage_munch.mk
@@ -11,9 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Inherit from alioth device
+# Inherit from Munch device
 $(call inherit-product, device/xiaomi/munch/device.mk)
 
+# Device identifiers. These must come after all inclusions.
 PRODUCT_NAME := lineage_munch
 PRODUCT_DEVICE := munch
 PRODUCT_MANUFACTURER := Xiaomi
